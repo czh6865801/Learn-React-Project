c8c83c315970c9f284c5904a15327406f4d58b41
czh6865801 <2251668603@qq.com>
2020-09-06 19:53:35 +0800

    修改

--- study_pro_0/src/Views/Layout/index.js
+++ study_pro_0/src/Views/Layout/index.js
@@ -1,10 +1,63 @@
 import React from 'react';
 import './index.less';
 
-function Layout() {
+function Layout(props) {
+  console.log(props)
   return (
-    <div className="qqq">
-      3123
+    <div className="layout">
+      <section className="layout-left">
+        <aside className="aside">
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+          可以移动<br />
+        </aside>
+      </section>
+      <section className="layout-right">
+        <div className="logo-icon">
+          <img className="logo-img" src="/Images/logo.svg" />
+          <h1 className="logo-name">Ant Design Pro</h1>
+        </div>
+        <header className="header">
+          1111
+        </header>
+        <main className="main">
+          <div className="main-body">
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+            可以移动<br />
+          </div>
+        </main>
+      </section>
     </div>
   )
 }
