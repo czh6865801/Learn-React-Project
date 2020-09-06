7370cd37a416a0978b8bd476d1433d2c8e94cf56
czh6865801 <2251668603@qq.com>
2020-09-06 23:45:49 +0800

    xiugai

--- study_pro_0/src/Views/Layout/index.js
+++ study_pro_0/src/Views/Layout/index.js
@@ -5,57 +5,57 @@ function Layout(props) {
   console.log(props)
   return (
     <div className="layout">
-      <section className="layout-left">
-        <aside className="aside">
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-          可以移动<br />
-        </aside>
-      </section>
-      <section className="layout-right">
+      <header className="layout-header">
         <div className="logo-icon">
           <img className="logo-img" alt="icon" src="/Images/logo.svg" />
           <h1 className="logo-name">Ant Design Pro</h1>
         </div>
-        <header className="header">
-          1111
-        </header>
-        <main className="main">
-          <div className="main-body">
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
-            可以移动<br />
+        <div className="header-menu">
+          首页
+        </div>
+        <div className="header-other">
+          退出
+        </div>
+      </header>
+      <section className="layout-section">
+        <aside className="aside">
+          <div className="aside-body">
+            <div className="aside-content">
+              <div>
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+              </div>
+            </div>
+            <div className="aside-footer">
+              21321
           </div>
+          </div>
+
+        </aside>
+        <main className="main">
+          可以移动<br />
+          可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
+                可以移动<br />
         </main>
       </section>
     </div>
